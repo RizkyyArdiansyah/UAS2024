58310d6ebe7f222be086f60c4eb04a07af0d6109
raketyonex <154741306+raketyonex@users.noreply.github.com>
2024-06-03 13:54:06 +0700

    Update delete_task.php

--- uas2024/delete_task.php
+++ uas2024/delete_task.php
@@ -1,21 +1,60 @@
-<?php
-session_start();
-// Koneksi ke database
-$conn = mysqli_connect("localhost", "root", "", "travel");
-
-// Periksa koneksi
-if (!$conn) {
-    die("Koneksi gagal: " . mysqli_connect_error());
+* {
+    margin: 0;
+    padding: 0;
+    font-family: Arial, Helvetica, sans-serif;
 }
 
-// Ambil task_id dari form
-$task_id = $_POST['task_id'];
+/* CSS PETA */
+#map {
+    width: 100%;
+    height: 100vh;
+    position: absolute;
+    z-index: 0;
+}
 
-// Hapus tugas dari database
-$sql = "DELETE FROM tasks WHERE id='$task_id'";
-mysqli_query($conn, $sql);
+/* CSS PopUp peringatan */
+#warning {
+    position: fixed;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.35);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    z-index: 1000;
+}
 
-// Redirect ke halaman utama
-header("Location: index.php");
-exit();
-?>
+#popup {
+    background-color: rgba(255, 255, 255, 0.9);
+    padding: 20px 25px;
+    text-align: justify;
+    max-width: 700px;
+    border-radius: 15px;
+}
+
+#popup h3 {
+    color: red;
+    font-size: 24px;
+    margin-bottom: 30px;
+    text-align: center;
+}
+
+#popup p {
+    font-size: 14px;
+    margin-bottom: 20px;
+}
+
+#popup button {
+    border: none;
+    padding: 8px 18px;
+    border-radius: 5px;
+    color: white;
+    background-color: rgb(38, 131, 244);
+    cursor: pointer;
+    display: block;
+    margin: 0 auto;
+}
+
+#popup button:hover {
+    background-color: rgb(57, 101, 224);
+}
