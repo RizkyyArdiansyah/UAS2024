686d77ce0f6c716c2fd29a00db98d860dfadb291
raketyonex <154741306+raketyonex@users.noreply.github.com>
2024-06-05 14:54:39 +0700

    Create kategori.php

---
+++ Views/templates/kategori.php
@@ -0,0 +1,9 @@
+<!-- Kartu Kategori -->
+<div class="card">
+    <h4>Pilih Kategori</h4>
+    <select id="kategori">
+        <option value="semua">Semua</option>
+        <option value="flora">Flora</option>
+        <option value="fauna">Fauna</option>
+    </select>
+</div>
