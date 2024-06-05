4600d4956e0f729d38117cda4377e2d2025ba8a1
raketyonex <154741306+raketyonex@users.noreply.github.com>
2024-06-05 14:55:10 +0700

    Create warning.php

---
+++ Views/templates/warning.php
@@ -0,0 +1,11 @@
+<!-- bagian warning -->
+<div id="warning">
+    <div id="popup">
+        <h3>! PERINGATAN !</h3>
+        <p>Dengan berpedoman pada <b>Undang-undang (UU) No. 5 Tahun 1990.</b> Mengenai konservasi sumber daya alam Hayati dan Ekosistemnya. Kami secara tegas menolak serta mengutuk segala bentuk tindakan ilegal yang berhubungan dengan ekosistem alam di Indonesia.</p>
+        <p>Website ini ditujukan untuk memberikan informasi mengenai flora & fauna endemik yang ada di Indonesia. Sebagai pengunjung, mungkin Anda bertanya tentang tanggung jawab dan kewajiban kami terkait dengan informasi yang disediakan website ini. Kami dengan tegas menyatakan bahwa tidak bertanggung jawab atas tindakan yang dilakukan oleh oknum yang menggunakan informasi dari website ini untuk tujuan ilegal.</p>
+        <p>Kami mengajak semua pengunjung untuk menjadi bagian dari upaya pelestarian dan perlindungan alam dengan cara yang legal dan etis. Edukasi, konservasi habitat, dan dukungan terhadap upaya pelestarian merupakan langkah-langkah yang lebih baik daripada melakukan praktik ilegal. Kami senang bisa menjadi sumber informasi yang berguna bagi Anda.</p>
+        <p><b>Terima kasih atas kunjungan Anda.</b></p>
+        <button onclick="Mengerti()">Mengerti</button>
+    </div>
+</div>
